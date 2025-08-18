95592bfe6cbab08a1a0813cb75b40e27a3014335
Nain046 <hasnaindar1234@gmail.com>
2025-08-18 14:17:24 +0500

    changes

--- script.js
+++ script.js
@@ -1,28 +1,34 @@
-// function checkevenodd(){
-//     let num=prompt("Enter number");
-//     num= Number(num);
-//     if (num % 2 == 0) {
-//         console.log("Even")
-//     }
-//     else{
-//         console.log("Odd")
-//     }
-// }
-// checkevenodd();
+// // function checkevenodd(){
+// //     let num=prompt("Enter number");
+// //     num= Number(num);
+// //     if (num % 2 == 0) {
+// //         console.log("Even")
+// //     }
+// //     else{
+// //         console.log("Odd")
+// //     }
+// // }
+// // checkevenodd();
 
 
-// date and time
+// // date and time
 
 
-var days=["sun", "mon", "tue", "wed", "thu","fri", "sat"]
-var today = new Date()
-console.log(today.getDay())
+// // var days=["sun", "mon", "tue", "wed", "thu","fri", "sat"]
+// // var today = new Date()
+// // console.log(today.getDay())
 
-var days=["sun", "mon", "tue", "wed", "thu","fri", "sat"]
-var today = new Date()
-console.log(days[today.getDay()])
+// // var days=["sun", "mon", "tue", "wed", "thu","fri", "sat"]
+// // var today = new Date()
+// // console.log(days[today.getDay()])
 
-var days=["sun", "mon", "tue", "wed", "thu","fri", "sat"]
-var today = new Date()
-console.log(today.getHours())
+// // var days=["sun", "mon", "tue", "wed", "thu","fri", "sat"]
+// // var today = new Date()
+// // console.log(today.getHours())
 
+// var customdate = new Date("20 jan 2020")
+// console.log(customdate)
+
+var customdate = new Date("20 jan 2020")
+var currentdate = new Date
+console.log(currentdate-customdate)
