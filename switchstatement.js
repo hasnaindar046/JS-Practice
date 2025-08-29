569164f4c8e0eeae0ab2c6d985efef4db43507ce
Nain046 <hasnaindar1234@gmail.com>
2025-08-29 13:25:59 +0500

    switch case

---
+++ switchstatement.js
@@ -0,0 +1,14 @@
+var day = prompt("Enter a day");
+switch (day) {
+  case "Monday":
+    console.log("Today is monday");
+    break;
+  case "Tuesday":
+    console.log("Today is tuesday");
+    break;
+  case "wednesday":
+    console.log("Today is wednesday");
+    break;
+    default:
+    alert("Today is offday")
+}
